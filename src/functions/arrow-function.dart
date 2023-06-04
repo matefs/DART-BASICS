b68994465f5efa211b2998ba4ec4c522d4bb7724
Mateus Schverz <mate_fs@hotmail.com>
2023-06-04 17:51:24 -0300

    add: comment to assert function

--- src/functions/arrow-function.dart
+++ src/functions/arrow-function.dart
@@ -1,6 +1,8 @@
 void main() {
   var loudify = (msg) => '!!! ${msg.toUpperCase()} !!!';
-  assert(loudify('hello') == '!!! HELLO !!!');
+
+  assert(loudify('hello') ==
+      '!!! HELLO !!!'); // validate, if it's false, it gona throw an error
 
   print(loudify('mensagem'));
 }
