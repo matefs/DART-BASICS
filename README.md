a37ebd84ab4dc2c41f723277868135bc884858e2
Mateus Schverz <mate_fs@hotmail.com>
2023-06-04 17:28:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DART-BASICS
+ Basic dart syntax 
---
+++ functions.dart
@@ -0,0 +1,50 @@
+/*
+Sintax: 
+return_type func_name (parameter_list):  
+{  
+    //statement(s)  
+   return value;  
+}  
+
+--- 
+
+fun_name(<argument_list>);  
+or  
+variable = function_name(argument);  
+
+ */
+
+// boolean
+bool isEven(int number) {
+  return number % 2 == 0;
+}
+
+// ints
+int? addAndPrint(int? a, int? b) {
+  if (a == null || b == null) {
+    return null;
+  }
+  int c = a + b;
+  print("The sum is: $c");
+  return c;
+}
+
+// Strings
+String getName() {
+  String a = 'Mateus';
+  return a;
+}
+
+void main() {
+  print('Is even ? ${isEven(10)}');
+
+  addAndPrint(1, 2);
+
+  print('Hello ${getName()}');
+
+  // IIFE - Immediately Invoked Function Expression
+  (() {
+    final segundoNome = 'José';
+    print('Hello friend ${segundoNome}');
+  })();
+}
